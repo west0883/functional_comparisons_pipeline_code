ec22a01c12388f63ff20fa13506bac87c3dc1a7b
Sarah West <west0883@umn.edu>
2022-05-26 09:37:57 -0500

    Remove full onsets/offsets from plots.

--- plot_starts_and_stops_together.m
+++ plot_starts_and_stops_together.m
@@ -28,7 +28,6 @@ function [] = plot_starts_and_stops_together(parameters)
         % Do starts 
         load_dir_string = CreateStrings(parameters.spontaneous_periods_dir, keywords, values);
         load([load_dir_string 'values_average.mat'], 'values_average');
-        full_onset = values_average{195}(1:20,:);
         startwalk = values_average{192}(1:20,:);
 
         load_dir_string = CreateStrings(parameters.motorized_periods_dir, keywords, values);
@@ -37,17 +36,15 @@ function [] = plot_starts_and_stops_together(parameters)
         m_start_800 = values_averaged.x800(1:20,:);
 
         figure; 
-        subplot(4, 50, 10:40); imagesc(full_onset); caxis([-2 2]); title('full onset');
-        subplot(4, 50, 50 + (26:34)); imagesc(startwalk); caxis([-2 2]); title('startwalk'); 
-        subplot(4, 50, 100 + (26:50)); imagesc(m_start_400);  caxis([-2 2]); title('motorized start, accel 400');
-        subplot(4, 50, 150 + (26:50)); imagesc(m_start_800);  caxis([-2 2]); title('motorized start, accel 800');
+        subplot(3, 24, 1:8); imagesc(startwalk); caxis([-2 2]); title('startwalk'); 
+        subplot(3, 1, 2); imagesc(m_start_400);  caxis([-2 2]); title('motorized start, accel 400');
+        subplot(3, 1, 3); imagesc(m_start_800);  caxis([-2 2]); title('motorized start, accel 800');
         sgtitle(['PCA scores individual mouse, ' strjoin(values(1:numel(values)/2), ', ')]);
         savefig([output_dir 'all_starts_together.fig']);
 
         % Do stops
         load_dir_string = CreateStrings(parameters.spontaneous_periods_dir, keywords, values);
         load([load_dir_string 'values_average.mat'], 'values_average');
-        full_offset = values_average{196}(1:20, :);
         stopwalk = values_average{193}(1:20, :);
     
         load_dir_string = CreateStrings(parameters.motorized_periods_dir, keywords, values);
@@ -55,11 +52,10 @@ function [] = plot_starts_and_stops_together(parameters)
         m_stop_400 = values_averaged.x400(1:20,:);
         m_stop_800 = values_averaged.x800(1:20,:);
 
-        figure; 
-        subplot(4, 50, 10:40); imagesc(full_offset);  caxis([-2 2]); title('full offset');
-        subplot(4, 50, 50 + (17:25)); imagesc(stopwalk);  caxis([-2 2]); title('stopwalk'); 
-        subplot(4, 50, 100 + (1:25)); imagesc(m_stop_400);  caxis([-2 2]); title('motorized stop, accel 400');
-        subplot(4, 50, 150 + (1:25)); imagesc(m_stop_800);  caxis([-2 2]); title('motorized stop, accel 800');
+        figure;
+        subplot(3, 24, 17:24); imagesc(stopwalk);  caxis([-2 2]); title('stopwalk'); 
+        subplot(3, 1, 2); imagesc(m_stop_400);  caxis([-2 2]); title('motorized stop, accel 400');
+        subplot(3, 1, 3); imagesc(m_stop_800);  caxis([-2 2]); title('motorized stop, accel 800');
         sgtitle(['PCA scores individual mouse, ' strjoin(values(1:numel(values)/2), ', ')]);
         savefig([output_dir 'all_stops_together.fig']);
     end 
